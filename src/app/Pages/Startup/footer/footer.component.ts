3c16c3abed97343a8b8e51077f7b5921b623feac
Satish Jadhav <sj8390613529@gmail.com>
2024-12-05 21:48:53 +0530

    working on responsive

--- src/app/Pages/Startup/footer/footer.component.ts
+++ src/app/Pages/Startup/footer/footer.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { StartupService } from '../startup.service';
+import { Category } from '../../../Shared/Models/Category';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  readonly startup = inject(StartupService);
+  router = inject(Router);
+  categories: Category[] = [];
+  ngOnInit() {
+    this.startup.getCategories().subscribe((res) => {
+      console.log(res);
+      this.categories = res.slice(0, 10);
+    });
+  }
 
+  onCategory(category: Category) {
+    localStorage.setItem('CateImage', category.Category_Image);
+    this.router.navigate(['product-list/' + category.Category_ID]);
+  }
 }
